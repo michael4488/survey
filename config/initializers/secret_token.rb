bfa187c7d25228866252e3474dad9ab8d46bf988
michael <michael@simplee.com>
2014-05-03 18:13:21 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Survey::Application.config.secret_key_base = '6d4e4370cefe71a79d940e1dea6dfe6ec09830f3fdfb60efe88ac054db3a6f70dbfa9ce95f17a8eb4a9c0003a8feb8d202065b766fa0419e29f0324c626958ea'
